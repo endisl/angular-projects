ae9f0c91aa116117e05a1bbc159ac1ffe5e0cf0a
endisl <endiluamba@gmail.com>
2022-10-12 23:55:11 -0300

    flashcards

--- flashcards/src/app/app.module.ts
+++ flashcards/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlashComponent } from './flash/flash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlashComponent
   ],
   imports: [
     BrowserModule,
