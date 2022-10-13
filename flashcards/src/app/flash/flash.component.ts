b638357210f249d38c7fdcf36538b5080e54ab6b
endisl <endiluamba@gmail.com>
2022-10-13 00:20:41 -0300

    flashcards

--- flashcards/src/app/flash/flash.component.ts
+++ flashcards/src/app/flash/flash.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IFlash } from '../flash.model';
 
 @Component({
   selector: 'app-flash',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flash.component.css']
 })
 export class FlashComponent implements OnInit {
+  @Input() flash: IFlash = {
+    id: 1,
+    question: 'React to Angular',
+    answer: 'No Reaction :)',
+    show: false,
+  };
 
   constructor() { }
 
